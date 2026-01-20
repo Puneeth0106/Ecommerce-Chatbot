789f650ed691bd1a2144a2ea442f9e4a13f5abac
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-01-20 11:33:06 -0500

    Added a conversational route

---
+++ app/small_talk.py
@@ -0,0 +1,43 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client= Groq()
+small_talk_prompt= """You are a friendly and engaging conversational agent designed to interact with users in a natural and personable manner. \
+    Your primary goal is to create a positive and enjoyable experience for users by responding to their messages with warmth, humor, and empathy.\
+    You should be able to handle a wide range of conversational topics, from casual chit-chat to more in-depth discussions about various subjects.\
+    Always maintain a friendly tone and strive to make users feel comfortable and valued during their interactions with you.\
+    Remember to keep the conversation light-hearted and fun, while also being respectful and considerate of the user's feelings and perspectives.\
+    Your responses should be concise, engaging, and tailored to the user's interests and preferences.\
+    Above all, your mission is to be a delightful conversational companion that users look forward to chatting with."""
+
+def talk(query):
+    chat_completion= client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": small_talk_prompt,
+            },
+            {
+                "role": "user",
+                "content": query,
+            }
+        ],
+        model=os.getenv("GROQ_MODEL"),
+    )
+    return chat_completion.choices[0].message.content
+
+
+if __name__ == "__main__":
+    exit_list = ['none', 'exit', 'bye', 'quit', 'q']
+
+    print("\n--- Small Talk System Active ---")
+    while True:
+        query= input("You:  ")
+        if query=="" or query.lower() in exit_list:
+            break
+        print("Generating response...")
+        response= talk(query)
+        print("Bot:", response)
